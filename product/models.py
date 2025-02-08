1828ae18f8cb96cd6564efcda5a0f55a34be24ac
mhdimohammadi <mohammadmahdi11121382@gmail.com>
2025-02-08 23:09:20 +0330

    adding product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    class Genre(models.TextChoices):
+        ACTION = 'ACTION', 'ACTION'
+        ADVENTURE = 'ADVENTURE', 'ADVENTURE'
+        FAMILY = 'FAMILY', 'FAMILY'
+        PUZZLE = 'PUZZLE', 'PUZZLE'
+        SHOOTER = 'SHOOTER', 'SHOOTER'
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    game_id = models.CharField(max_length=20,unique=True)
+    genre = models.CharField(max_length=20,choices=Genre.choices)
+    image = models.ImageField(upload_to="images/games")
+
+
